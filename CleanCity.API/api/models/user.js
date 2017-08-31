c8e0c2f3d9e5019c1ac41534a7315abf3cac1904
IDOMNICH\Ivan Domnich <domnich.ivan@gmail.com>
2017-08-31 13:47:31 +0300

    implementation

--- CleanCity.API/api/models/user.js
+++ CleanCity.API/api/models/user.js
@@ -56,6 +56,10 @@ UserSchema.methods.verifyPassword = function(password, cb) {
         if (err) return cb(err);
         cb(null, isMatch);
     });
+    bcrypt.compare(password, this.passwordConf, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
 };
 
 module.exports = mongoose.model('User', UserSchema);
